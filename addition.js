621452d322c93d7b3fbd18a6378bbce8b899b6f3
Shahnewaz Alam <181187140+heysanzu@users.noreply.github.com>
2026-01-05 10:05:32 +0530

    Add two file addition.js and funAddition.js

---
+++ addition.js
@@ -0,0 +1,9 @@
+// Assigning values to variables
+const num1 = 10;
+const num2 = 20;
+
+// Adding the numbers
+const sum = num1 + num2;
+
+// Outputting the result to the console
+console.log("The sum is: " + sum);
---
+++ funAddition.js
@@ -0,0 +1,7 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Calling the function
+let result = addNumbers(15, 25);
+console.log("Result:", result);
